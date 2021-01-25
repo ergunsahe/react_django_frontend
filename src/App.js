9ae1216d464ee708afa39c34ad897803fd861d14
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2021-01-25 22:00:24 +0100

    add register page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
-import NavBar from "./components/Navbar"
-import Home from "./pages/HomePage"
-import DetailPage from "./pages/DetailPage"
+// import NavBar from "./components/Navbar"
+// import Home from "./pages/HomePage"
+// import DetailPage from "./pages/DetailPage"
+import AppRouter from "./router/Router"
 
 
 function App() {
   return (
     <div >
-      <NavBar/>
-      <DetailPage/>
-      
+      <AppRouter/>
     </div>
   );
 }
---
+++ src/router/Router.js
@@ -0,0 +1,33 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+import  DetailPage from "../pages/DetailPage";
+import  HomePage from "../pages/HomePage";
+import SignUp from "../pages/Register" 
+import  Navbar from "../components/Navbar"
+
+function AppRouter(params) {
+    return (
+        <Router>
+            <Navbar/>
+            <Switch>
+                <Route exact path="/home" component={HomePage} />
+                <Route exact path="/detail" component={DetailPage} />
+                <Route exact path="/register" component={SignUp} />
+                {/* <Route exact path="/forgot-password" component={ForgotPassword} /> */}
+                {/* <Route
+                    exact
+                    path="/user/:id"
+                    component={currentUser ? UserDetail : Signin}
+                />
+                <Route
+                    exact
+                    path="/user/:id/post"
+                    component={currentUser ? UserPost : Signin}
+                />
+                <Route path="/" component={Main} /> */}
+            </Switch>
+        </Router>
+    )    
+}
+
+export default AppRouter;
