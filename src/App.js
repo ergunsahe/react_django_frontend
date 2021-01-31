d3a1f653e3a570dfac736bb9be6dc8e66ad05333
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2021-01-31 01:17:11 +0100

    fixed too many render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-
-// import NavBar from "./components/Navbar"
-// import Home from "./pages/HomePage"
-// import DetailPage from "./pages/DetailPage"
 import AppRouter from "./router/Router"
 import AuthContextProvider from "./context/AuthContext";
 
